0aef785b56cccd3709c754c332cf790b9c738807
farres1 <sam.farrell@ons.gov.uk>
2025-05-28 14:31:00 +0100

    Fix lint issues

--- eq_cir_proxy_service/main.py
+++ eq_cir_proxy_service/main.py
@@ -1,3 +1,6 @@
-# Sample function to prevent checks failing - to be deleted when code is implemented
+"""Sample function that adds two numbers, to prevent checks failing - to be deleted when code is implemented."""
+
+
 def add_numbers(x, y):
+    """Function to add two numbers together."""
     return x + y
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,9 @@
+"""Sample test to prevent checks failing - to be deleted when further tests are implemented."""
+
 from eq_cir_proxy_service.main import add_numbers
 
 
-# Sample test to prevent checks failing - to be deleted when further tests are implemented
 def test_add_numbers():
-    assert add_numbers(2, 3) == 5
+    """Tests that two numbers are added together correctly."""
+    expected_result = 5
+    assert add_numbers(2, 3) == expected_result
