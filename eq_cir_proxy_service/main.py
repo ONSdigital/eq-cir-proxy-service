66777e8baf64bf48330042b81da6bbb2aa4a82e8
farres1 <sam.farrell@ons.gov.uk>
2025-05-28 14:46:04 +0100

    Add type hints

--- eq_cir_proxy_service/main.py
+++ eq_cir_proxy_service/main.py
@@ -1,6 +1,6 @@
 """Sample function that adds two numbers, to prevent checks failing - to be deleted when code is implemented."""
 
 
-def add_numbers(x, y):
+def add_numbers(x: int, y: int) -> int:
     """Function to add two numbers together."""
     return x + y
