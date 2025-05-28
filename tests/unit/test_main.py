6c233fbd7c5b2eb7a673ecfce14bc103b4fee684
farres1 <sam.farrell@ons.gov.uk>
2025-05-28 13:55:12 +0100

    Format files

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,6 @@
 from eq_cir_proxy_service.main import add_numbers
 
+
 # Sample test to prevent checks failing - to be deleted when further tests are implemented
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
-
