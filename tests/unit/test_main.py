83d01c1731d388b51603483faf3509c004a2548a
farres1 <sam.farrell@ons.gov.uk>
2025-06-24 08:06:07 +0100

    Update test docstring

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,4 +1,4 @@
-"""Sample test to prevent checks failing - to be deleted when further tests are implemented."""
+"""Tests for the main application module."""
 
 from fastapi.testclient import TestClient
 
