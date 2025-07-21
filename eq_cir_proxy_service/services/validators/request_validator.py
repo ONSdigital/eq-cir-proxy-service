7f7985060cad528fae262bd01608d9f5d904bcf8
Paul-Joel <paul.joel@ons.gov.uk>
2025-07-21 12:25:55 +0100

    Validate with semver

--- eq_cir_proxy_service/services/validators/request_validator.py
+++ eq_cir_proxy_service/services/validators/request_validator.py
@@ -2,8 +2,7 @@
 
 from __future__ import annotations
 
-import re
-
+import semver
 from fastapi import HTTPException, status
 
 from eq_cir_proxy_service.config.logging_config import logging
@@ -18,14 +17,11 @@ def validate_version(version: str) -> None:
     Parameters:
     - version: The version to validate.
     """
-    pattern = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$"
-
-    if re.match(pattern, version):
-        logger.info("The %s version matches the pattern", version)
-
-    else:
-        logger.error("Invalid version %s", version)
+    try:
+        semver.VersionInfo.parse(version)
+    except ValueError as exception:
+        logger.exception("Invalid version: %s", version)
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={"status": "error", "message": exception_messages.EXCEPTION_400_INVALID_VERSION},
-        )
+        ) from exception
