2ff28f7e9d79eee4f1814bcfdfdc7544f86da358
oms09 <omar.moulana@ons.gov.uk>
2025-11-19 14:59:29 +0000

    lint

--- eq_cir_proxy_service/main.py
+++ eq_cir_proxy_service/main.py
@@ -32,7 +32,6 @@ async def root() -> dict:
 @app.get("/status")
 async def health_check() -> dict:
     """Health check endpoint for Cloud Run."""
-
     logger.info("Health check endpoint.")
     return {"status": "OK"}
 
