0b6e6aab83f97031a1fc5f39fe172d1e96d5215a
Paul-Joel <paul.joel@ons.gov.uk>
2025-12-10 17:24:09 +0000

    Fix linting issues

--- eq_cir_proxy_service/types/custom_types.py
+++ eq_cir_proxy_service/types/custom_types.py
@@ -1,7 +1,9 @@
 """This module defines custom types used in the EQ CIR Proxy Service."""
 
+from collections.abc import Mapping, Sequence
+
 # Define the type for the instrument
 Instrument = dict[str, bool | int | str | list | object]
 
-# Define the type for the instrument
-InstrumentMetadata = list[dict[str, bool | int | str]]
+# Define the type for the instrument metadata
+InstrumentMetadata = Sequence[Mapping[str, bool | int | str]]
